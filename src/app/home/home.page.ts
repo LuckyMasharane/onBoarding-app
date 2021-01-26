13d655b8e2272797ca34984695397814c3b927f1
lucky masharane <lmasharane@gmail.com>
2021-01-26 13:04:59 +0200

    onboarding

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonSlides } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+
+  onboardSlides = [];
+  @ViewChild('mainSlides', { static: true}) slides: IonSlides
 
   constructor() {}
+
+  ngOnInit(){
+    this.onboardSlides = [
+      {
+        title: 'Enjoying the Moment',
+        img: 'slide_1',
+        desc: 'Most people learn to make every moment count by simple going out'
+      },
+      {
+        title: 'live the life',
+        img: 'slide_2',
+        desc: 'In our daily lives, We offer good time and good moment to make better life'
+      },
+      {
+        title:'capture the moment',
+        img: 'slide_3',
+        desc: 'You not alone, Dont miss a good moment'
+      }
+    ]
+  }
+
+  goBack(){
+    this.slides.slidePrev();
+  }
+  skipBtn(){
+    console.log('i will go to home');
+    
+  }
+  goNext(){
+    this.slides.slideNext();
+  }
 
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyBsdMnKmlnVSqJXDqkizq1vGfnKHUFDucs",
+    authDomain: "onboarding-app-21729.firebaseapp.com",
+    projectId: "onboarding-app-21729",
+    storageBucket: "onboarding-app-21729.appspot.com",
+    messagingSenderId: "682251098365",
+    appId: "1:682251098365:web:f942c38eecb64eeb638694"
+  }
 };
